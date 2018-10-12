c5f7eeeafaa3a0183e0d1f171698c8171f55845c
Laurent Humbert <Laurent.Humbert@codecomputerlove.com>
2018-10-12 15:23:37 +0100

    Removed commented out code

--- src/components/pages/GuildDrawingsPage/index.js
+++ src/components/pages/GuildDrawingsPage/index.js
@@ -35,26 +35,6 @@ const GuildDrawingsPage = () => {
             </div>
           )
         })}
-        {/* <div>
-          <LazyImage className="marks-drawing__img" dataSrc="/media/images/best druid.png" alt="mad paint skills" />
-          <p className="guild-drawings__heading">Made by Joe, age 19</p>
-        </div>
-        <div>
-          <LazyImage className="marks-drawing__img" dataSrc="/media/images/fetid strat by joe.png" alt="#joe4RL" />
-          <p className="guild-drawings__heading">Made by Joe, age 19</p>
-        </div>
-        <div>
-          <LazyImage className="marks-drawing__img" dataSrc="/media/images/marks drawing 3.png" alt="mark is an artist" />
-          <p className="guild-drawings__heading">Made by Mark, age 24</p>
-        </div>
-        <div>
-          <LazyImage className="marks-drawing__img" dataSrc="/media/images/mark drawing 2.png" alt="mark is an artist" />
-          <p className="guild-drawings__heading">Made by Mark, age 24</p>
-        </div>
-        <div>
-          <LazyImage className="marks-drawing__img" dataSrc="/media/images/marks drawing.png" alt="mark is an artist" />
-          <p className="guild-drawings__heading">Made by Mark, age 23</p>
-        </div> */}
       </div>
     </PageTemplate>
   )
